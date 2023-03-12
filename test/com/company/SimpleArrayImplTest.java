ff925ff7df03f35111ddd630a1b0e97f4186a472
Ulya Ivancova <ulya-rubo@mail.ru>
2023-03-13 00:49:10 +0200

    Commit-Test

--- test/com/company/SimpleArrayImplTest.java
+++ test/com/company/SimpleArrayImplTest.java
@@ -1,14 +1,20 @@
 package com.company;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SimpleArrayImplTest {
-    
+    Simple<String> newArray = new SimpleArrayImpl<>();
+
 
     @Test
     public void add() {
+        newArray.add("One");
+        newArray.add("Two");
+        newArray.add("Three");
+        assertTrue(newArray.getSize()==3);
     }
 
     @Test
@@ -17,14 +23,27 @@ public class SimpleArrayImplTest {
 
     @Test
     public void delete() {
+        newArray.add("One");
+        newArray.add("Two");
+        newArray.delete(1);
+        assertTrue(newArray.getSize()==1);
     }
 
     @Test
     public void getElement() {
+        newArray.add("One");
+        newArray.add("Two");
+        String expected = newArray.getElement(1);
+        String actual = "Two";
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void deleteAllElement() {
+        newArray.add("One");
+        newArray.add("Two");
+        newArray.deleteAllElement();
+        assertTrue(newArray.getSize()==0);
     }
 
     @Test
@@ -33,6 +52,12 @@ public class SimpleArrayImplTest {
 
     @Test
     public void getSize() {
+        newArray.add("One");
+        newArray.add("Two");
+        newArray.add("Three");
+        int expected = newArray.getSize();
+        int actual = 3;
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
